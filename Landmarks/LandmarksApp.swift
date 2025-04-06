825ff945b20659a3b30dc224b82d29ac80dc574c
Ali Shafiei <999alishafiei999@gmail.com>
2025-04-06 08:44:19 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ali Shafiei on 06.04.25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
